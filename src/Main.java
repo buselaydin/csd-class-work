cb2f7d1e4418fee8a2f549d6e9d2336177c481b2
Busel Aydın <71033829+buselaydin@users.noreply.github.com>
2023-11-03 00:32:22 +0300

    padTrailing and padLeading Solution

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
+import test.PadLeadingTrailingTest;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        PadLeadingTrailingTest.run();
     }
 }
---
+++ src/utils/StringUtil.java
@@ -0,0 +1,44 @@
+package utils;
+
+public class StringUtil {
+
+    public static String padLeading(String s, int len, char ch)
+    {
+        int length = s.length();
+
+        return len <= length ? s : repeat(len - length, ch) + s;
+
+    }
+    public static String padLeading(String s, int len)
+    {
+        return padLeading(s, len, ' ');
+    }
+    public static String padTrailing(String s, int len, char ch)
+    {
+        int length = s.length();
+
+        for (int i = 0; length < len && i < len - length; ++i)
+            s = s + ch;
+
+        return s;
+    }
+    public static String padTrailing(String s, int len)
+    {
+        return padTrailing(s, len, ' ');
+    }
+
+    public static String repeat(int count, String s)
+    {
+        String str = "";
+
+        while (count-- > 0)
+            str += s;
+
+        return str;
+    }
+
+    public static String repeat(int count, char ch)
+    {
+       return repeat(count, ch + "");
+    }
+}
