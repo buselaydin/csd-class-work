a5128f523f116467b3fe6333165cc6a409d2a5f5
Busel Aydın <71033829+buselaydin@users.noreply.github.com>
2023-11-26 22:39:47 +0300

    getShortestPangram added to StringUtil

--- src/utils/StringUtil.java
+++ src/utils/StringUtil.java
@@ -40,4 +40,68 @@ public class StringUtil {
     {
        return repeat(count, ch + "");
     }
+
+    public static String getShortestPangramEN(String s)
+    {
+        String result = s;
+
+        int end = s.length();
+
+        while (end != 0) {
+            int begin = 0;
+
+            while (begin != end) {
+                String str = s.substring(begin++, end);
+
+                if (str.length() >= 26 && isPangramEN(str) && str.length() < result.length())
+                    result = str;
+
+            }
+
+            --end;
+        }
+        return result;
+    }
+    public static String getShortestPangramTR(String s)
+    {
+        String result = s;
+
+        int end = s.length();
+
+        while (end != 0) {
+            int begin = 0;
+
+            while (begin != end) {
+                String str = s.substring(begin++, end);
+
+                if (str.length() >= 29 && isPangramTR(str) && str.length() < result.length())
+                    result = str;
+            }
+
+            --end;
+        }
+
+        return result;
+    }
+
+    public static boolean isPangramTR(String s)
+    {
+        return isPangram(s.toLowerCase(), "abcçdefgğhıijklmnoöprsştuüvyz");
+    }
+
+    public static boolean isPangramEN(String s)
+    {
+        return isPangram(s.toLowerCase(), "abcdefghijklmnopqrstuwxvyz");
+    }
+
+    public static boolean isPangram(String s, String alphabet)
+    {
+        int len = alphabet.length();
+
+        for (int i = 0; i < len; ++i)
+            if (s.indexOf(alphabet.charAt(i)) == -1)
+                return false;
+
+        return true;
+    }
 }
